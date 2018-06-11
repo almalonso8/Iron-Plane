9a6887b8bae05a5ad21dccf7a78e0f19c9f1d930
Alma Alonso <alma.aa93@gmail.com>
2018-06-11 07:46:50 +0200

    scaling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = function(){
     var canvas = document.createElement("canvas");
 
-    canvas.width = 1580;
-    canvas.height = 800;
+    canvas.width = 1200;
+    canvas.height = 600;
 
     document.body.prepend(canvas);
 
